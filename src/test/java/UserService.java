cde280fddc04d707d04f6c2e9e46f96f6717fab9
Lijuce <441214473@qq.com>
2021-06-20 15:15:52 +0800

    加上多线程并发模拟所需的依赖

--- src/test/java/UserService.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-public interface UserService {
-    User queryUser(String userName,String password);
-    int addUser(User user);
-    void insertUsers(List<User> users);
-}
