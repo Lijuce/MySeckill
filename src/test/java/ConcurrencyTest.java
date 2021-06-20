63832e6cf2606b881a76afa28610a6b4f3a20dbb
Lijuce <441214473@qq.com>
2021-06-20 15:23:21 +0800

    添加多线程模拟测试类

--- src/test/java/ConcurrencyTest.java
+++ src/test/java/ConcurrencyTest.java
@@ -1,6 +1,12 @@
 
-import com.dto.Exposer;
+import com.Utility.ConstValue;
+import com.Utility.CookieUtility;
 import com.service.SeckillService;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,37 +14,20 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
-import java.net.CookieStore;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.http.impl.client.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 //告诉junit spring的配置文件
-@ContextConfiguration({"classpath:spring-dao.xml", "classpath:spring-service.xml"})
-public class Tester {
+@ContextConfiguration({"classpath:/spring/spring-*.xml"})
+public class ConcurrencyTest {
     @Autowired
     SeckillService secKillService;
-    @Autowired
-    UserService userService;
-    private Exposer CookieUtility;
-
-    /**
-     * 插入用户
-     * @throws InterruptedException
-     */
-    //@Test
-    public void insertUser() throws InterruptedException {
-        List<User> users=new ArrayList<>(2000);
-        for (int i=500;i<2000;++i){
-            User user=new User("user"+i,"12345");
-            users.add(user);
-        }
-        userService.insertUsers(users);
-    }
 
     /**
      *测试并发的入口
@@ -61,13 +50,13 @@ public class Tester {
     public void calculateTime(int userNum) throws InterruptedException {
         long startTime=System.currentTimeMillis();
         ExecutorService service= Executors.newFixedThreadPool(userNum);
-        for (int i=0;i<userNum;++i){
-            final int num=i;
+        for (int i = 0; i < userNum; i++){
+            int finalI = i;
             service.execute(new Thread(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        request(num);
+                        request(13500000000l+ finalI);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -80,22 +69,14 @@ public class Tester {
         System.out.println("耗费时间："+(endTime-startTime)/1000);
     }
 
-    /**
-     * 请求
-     * @param i
-     * @throws IOException
-     */
-    public void  request(int i) throws IOException {
-        /**
-         * 耗时统计：500并发10秒
-         */
+    public void  request(long phone) throws IOException {
         //高并发请求测试
-        int secID=1001;
-        String urlPath = "http://localhost:8080/SecondKill/meteor/"+secID+"/"
+        int secID=1000;
+        String urlPath = "http://localhost:8080/seckill/"+secID+"/"
                 +CookieUtility.getMd5(secID)+"/execution";
-        String userKey = "user"+i;
+        long userKey = phone;
         String result = "";
-        CookieStore cookieStore = new BasicCookieStore();
+        BasicCookieStore cookieStore = new BasicCookieStore();
         CloseableHttpClient httpClient = HttpClients.custom()
                 .setDefaultCookieStore(cookieStore)
                 .build();
@@ -103,11 +84,13 @@ public class Tester {
         try {
             HttpPost post = new HttpPost(urlPath);//这里发送post请求
             post.setConfig(requestConfig);
+            // 每个用户的cookie创建
             List<BasicClientCookie> cookies=createCookie(userKey);
-            for (BasicClientCookie cookie:cookies)
+            for (BasicClientCookie cookie:cookies){
                 cookieStore.addCookie(cookie);
+            }
             // 通过请求对象获取响应对象
-            HttpResponse response = httpClient.execute(post);
+            CloseableHttpResponse response = httpClient.execute(post);
             // 判断网络连接状态码是否正常(0--200都数正常)
             result = EntityUtils.toString(response.getEntity(), "utf-8");
         } catch (Exception e) {
@@ -116,19 +99,34 @@ public class Tester {
         }
     }
 
-
     List<BasicClientCookie> createCookie(String userName){
         List<BasicClientCookie> cookies=new ArrayList<>();
         //用户名
         BasicClientCookie cookie = new BasicClientCookie(ConstValue.USER_KEY, userName);
         cookie.setDomain("localhost");
-        cookie.setPath("/SecondKill/");
+        cookie.setPath("/seckill/");
         cookies.add(cookie);
         //ssid
-        BasicClientCookie ssID = new BasicClientCookie(ConstValue.SS_ID,CookieUtility.getMd5(userName));
+        BasicClientCookie ssID = new BasicClientCookie(ConstValue.SS_ID, CookieUtility.getMd5(userName));
         ssID.setDomain("localhost");
-        ssID.setPath("/SecondKill/");
+        ssID.setPath("/seckill/");
         cookies.add(cookie);
+        // 手机号码
+        BasicClientCookie phone = new BasicClientCookie(ConstValue.USER_KEY, userName);
+        cookie.setDomain("localhost");
+        cookie.setPath("/seckill/");
+        cookies.add(cookie);
+        return cookies;
+    }
+
+    List<BasicClientCookie> createCookie(long phone){
+        List<BasicClientCookie> cookies=new ArrayList<>();
+
+        // 手机号码
+        BasicClientCookie phoneCookie = new BasicClientCookie(ConstValue.USER_Phone, String.valueOf(phone));
+        phoneCookie.setDomain("localhost");
+        phoneCookie.setPath("/seckill/");
+        cookies.add(phoneCookie);
         return cookies;
     }
 }
