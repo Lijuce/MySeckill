b608c059dc0b79629ab7222fcbe46a60a6db4abc
Lijuce <441214473@qq.com>
2021-06-20 15:17:20 +0800

    Cookie设置用的工具类

---
+++ src/main/java/com/Utility/ConstValue.java
@@ -0,0 +1,9 @@
+package com.Utility;
+
+public class ConstValue {
+    public static final String USER_KEY="UserKey";
+    public static final String SS_ID="SsID";
+    public static final String USER_Phone="userPhone";
+
+    public static final int SAVE_TIME=7*24*60*60;//Cookie保存时间7天
+}
